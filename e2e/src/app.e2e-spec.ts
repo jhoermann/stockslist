26ddd3ece199d35e5489a791a9cab6921a6441ea
Jochen Hörmann <jochen.hoermann@gmail.com>
2020-05-26 15:42:59 +0200

    Rename app to stockslist

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('stocklist app is running!');
+    expect(page.getTitleText()).toEqual('stockslist app is running!');
   });
 
   afterEach(async () => {

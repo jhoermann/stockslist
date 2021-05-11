cde8e39d9dcd3d0431f14becf8444d8cbbb8d4f8
Jochen Hörmann <jochen.hoermann@gmail.com>
2021-05-11 19:54:34 +0200

    Accounts E2E test

--- server/test/accounts.e2e-spec.ts
+++ server/test/accounts.e2e-spec.ts
@@ -1,26 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing'
+import { Test } from '@nestjs/testing'
 import { INestApplication, ValidationPipe } from '@nestjs/common'
 import * as request from 'supertest'
 import { AppModule } from './../src/app.module'
-import Database from './../src/database'
-import { AccountsService } from './../src/accounts/accounts.service'
 
 describe('AccountsController (e2e)', () => {
   let app: INestApplication
-  let accountsService: AccountsService
 
-  beforeAll(() => {
-    Database.initTestDb()
-  })
-
-  beforeEach(async () => {
-    accountsService = new AccountsService()
-    
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile()
+    })
+      .compile()
 
-    app = moduleFixture.createNestApplication()
+    app = moduleRef.createNestApplication()
     app.useGlobalPipes(new ValidationPipe({
       whitelist: true,
     }))
@@ -31,17 +23,6 @@ describe('AccountsController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/accounts')
       .expect(200)
-      .expect(accountsService.getAccounts())
-  })
-
-  it('gets the default Account', () => {
-    return request(app.getHttpServer())
-      .get('/accounts/1')
-      .expect(200)
-      .expect(accountsService.getAccount(1))
-  })
-
-  afterAll(() => {
-    Database.deleteTestDb()
+      .expect([])
   })
 })

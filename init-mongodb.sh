e400ac4396b21365ceccd1c5a4d0d10070fb9464
Jochen Hörmann <jochen.hoermann@gmail.com>
2021-05-18 20:26:32 +0200

    Create and connect to test database

--- init-mongodb.sh
+++ init-mongodb.sh
@@ -1,10 +1,15 @@
-mongo -- "$MONGO_INITDB_DATABASE" <<EOF
+mongo <<EOF
     var rootUser = '$MONGO_INITDB_ROOT_USERNAME';
     var rootPassword = '$MONGO_INITDB_ROOT_PASSWORD';
+    var user = '$MONGO_USERNAME';
+    var password = '$MONGO_PASSWORD';
+
     var admin = db.getSiblingDB('admin');
     admin.auth(rootUser, rootPassword);
 
-    var user = '$MONGO_INITDB_USERNAME';
-    var password = '$MONGO_INITDB_PASSWORD';
+    use $MONGO_INITDB_DATABASE
+    db.createUser({user: user, pwd: password, roles: ["readWrite"]});
+    
+    use $MONGO_DATABASE_TEST
     db.createUser({user: user, pwd: password, roles: ["readWrite"]});
 EOF
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common'
-import { TypeOrmModule } from '@nestjs/typeorm'
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm'
 import { AccountsModule } from './accounts/accounts.module'
+import dbConfigDefault from '../db-config'
+
+let dbConfig = dbConfigDefault
+if (process.env.NODE_ENV === 'test') {
+  dbConfig = { ...dbConfigDefault, database: 'stockslist-test' }
+}
 
 @Module({
   imports: [
     AccountsModule,
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot(dbConfig as TypeOrmModuleOptions),
   ],
 })
 export class AppModule {}
